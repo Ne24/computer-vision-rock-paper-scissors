e150bff4584778f1c7925aa2a13da0e3228359e0
code24z <qsmedia@gmx.com>
2023-01-23 18:24:56 +0000

    changes losing message

--- manual_rps.py
+++ manual_rps.py
@@ -20,11 +20,11 @@ def get_winner(computer_choice, user_choice):
     if computer_choice == user_choice:
         print("It is a tie!")
     elif computer_choice == "Rock" and user_choice == "Scissors":
-        print("You lost!")
+        print("You lost")
     elif computer_choice == "Paper" and user_choice == "Rock":
-        print("You lost!")
+        print("You lost")
     elif computer_choice == "Scissors" and user_choice == "Paper":
-        print("You lost!")
+        print("You lost")
     else:
         print("You won!")
     return
