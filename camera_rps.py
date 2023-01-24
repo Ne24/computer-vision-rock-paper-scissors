cdae425f8772a10296ccfaa4bfbf43597b3abf45
code24z <qsmedia@gmx.com>
2023-01-24 01:03:26 +0000

    Adds count down feature

--- camera_rps.py
+++ camera_rps.py
@@ -1,7 +1,10 @@
 """"""
+import time
 import cv2
 from keras.models import load_model
 import numpy as np
+
+STARTTIME = time.time()
 
 def get_prediction():
     """Gets prediction of the keras model for user hand gestures"""
@@ -10,7 +13,7 @@ def get_prediction():
     cap = cv2.VideoCapture(0)
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-    while True: 
+    while time.time() - STARTTIME < 3:
         ret, frame = cap.read()
         resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
         image_np = np.array(resized_frame)
@@ -28,3 +31,26 @@ def get_prediction():
     # Destroy all the windows
     cv2.destroyAllWindows()
     return prediction
+
+## create countdown function and call
+# def countdown_prediction():
+#     start_time = time.time()
+
+#     while start_time - time.time() < 3:
+#         prediction_list = get_prediction()
+#     pass
+#     return prediction_list
+
+# prediction_list = countdown_prediction()
+
+
+prediction_list = get_prediction()
+
+if prediction_list[0][0] > (prediction_list[0][1] or prediction_list[0][2]):
+    print("you chose rock")
+elif prediction_list[0][1] > (prediction_list[0][0] or prediction_list[0][2]):
+    print("you chose paper")
+elif prediction_list[0][0] > (prediction_list[0][1] or prediction_list[0][2]):
+    print("you chose scissors")
+else:
+    print("inconclusive input")
